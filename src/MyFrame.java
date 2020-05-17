c0f05b0551c1e04c4fb32ed7abae1cc21b8c5904
Benoniy <benranson5@gmail.com>
2020-05-17 14:07:00 +0100

    Start

---
+++ src/MyFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){
+        setTitle("Initiative server");
+        setSize(800,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        setVisible(true);
+    }
+}
