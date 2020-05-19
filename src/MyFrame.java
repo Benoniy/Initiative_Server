388dd19e35f1983aff7516411e733ec8582d0915
Benoniy <benranson5@gmail.com>
2020-05-19 14:44:45 +0100

    Started making swing components and adding them to the frame

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,6 +1,12 @@
+import UI_ELEMENTS.MyLabel;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
+    private JPanel content_panel;
+    static Color backgroundColor = new Color(240,240,240);
+
     MyFrame(){
         setTitle("Initiative server");
         setSize(800,600);
@@ -14,5 +20,20 @@ public class MyFrame extends JFrame {
 
         setVisible(true);
 
+        content_panel = new JPanel();
+        content_panel.setBackground(backgroundColor);
+        content_panel.setLayout(new GridBagLayout());
+        add(content_panel);
+        generate_content();
+    }
+
+    private void generate_content(){
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        content_panel.add(new MyLabel("A"), gbc);
     }
 }
